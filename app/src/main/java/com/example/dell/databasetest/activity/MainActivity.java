be3ae3945c6147bbb81ec75c95ad686a499eda65
kentanvictor <2030457926@qq.com>
2017-05-10 20:37:23 +0800

    SQLite存储数据

--- app/src/main/java/com/example/dell/databasetest/activity/MainActivity.java
+++ app/src/main/java/com/example/dell/databasetest/activity/MainActivity.java
@@ -1,9 +1,12 @@
-package com.example.dell.databasetest;
+package com.example.dell.databasetest.activity;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+
+import com.example.dell.databasetest.MyDatabaseHelper;
+import com.example.dell.databasetest.R;
 
 public class MainActivity extends AppCompatActivity {
     private MyDatabaseHelper dbHelper;
