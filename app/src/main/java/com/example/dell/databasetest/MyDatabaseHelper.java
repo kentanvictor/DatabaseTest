febb4b826cdb72dc803e1e010081f5e369b24fc5
kentanvictor <2030457926@qq.com>
2017-05-10 21:04:21 +0800

    SQLite存储数据（升级数据库）

--- app/src/main/java/com/example/dell/databasetest/MyDatabaseHelper.java
+++ app/src/main/java/com/example/dell/databasetest/MyDatabaseHelper.java
@@ -5,17 +5,21 @@ import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
 import android.widget.Toast;
 
-/**
+/*
  * Created by DELL on 2017/5/10.
  */
 
 public class MyDatabaseHelper extends SQLiteOpenHelper{
-    public static final String CREATE_BOOK = "create table Book("
+    private static final String CREATE_BOOK = "create table Book("
     + "id integer primary key autoincrement,"
             +"author text,"
             +"price real,"
             + "pages integer,"
     +"name text)";
+    private static final String CREATE_CATEGORY = "create table Category("
+    +"id integer primary key autoincrement,"
+            +"category_name text,"
+    +"category_code integer)";
     private Context mContext;
     public MyDatabaseHelper(Context context, String name, SQLiteDatabase.CursorFactory factory, int version) {
         super(context, name, factory, version);
@@ -25,11 +29,15 @@ public class MyDatabaseHelper extends SQLiteOpenHelper{
     @Override
     public void onCreate(SQLiteDatabase db) {
         db.execSQL(CREATE_BOOK);//调用exeSQL()方法去执行建表语句
+        db.execSQL(CREATE_CATEGORY);
         Toast.makeText(mContext, "Create succeeded", Toast.LENGTH_SHORT).show();
 
     }
 
     @Override
     public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+        db.execSQL("drop table if exists Book");
+        db.execSQL("drop table if exists Category");
+        onCreate(db);
     }
 }
--- app/src/main/java/com/example/dell/databasetest/activity/MainActivity.java
+++ app/src/main/java/com/example/dell/databasetest/activity/MainActivity.java
@@ -14,7 +14,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        dbHelper = new MyDatabaseHelper(this,"BookStore.db",null ,1);//构建MyDatabaseHelper对象
+        dbHelper = new MyDatabaseHelper(this,"BookStore.db",null ,2);//构建MyDatabaseHelper对象
         //上面对应的是，创建一个名字为"BookStore.db"并且版本号为1的数据库文件
         Button createDatabase = (Button) findViewById(R.id.create_database);
         createDatabase.setOnClickListener(new View.OnClickListener() {
