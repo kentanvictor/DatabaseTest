eacb40cf297f629cd9aeca98768f1043f339cef0
kentanvictor <2030457926@qq.com>
2017-05-13 17:18:36 +0800

    SQLite存储数据（查询数据，并且在日志中打印）(注解)

--- app/src/main/java/com/example/dell/databasetest/MyDatabaseHelper.java
+++ app/src/main/java/com/example/dell/databasetest/MyDatabaseHelper.java
@@ -3,7 +3,6 @@ package com.example.dell.databasetest;
 import android.content.Context;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
-import android.widget.Toast;
 
 /*
  * Created by DELL on 2017/5/10.
@@ -30,7 +29,7 @@ public class MyDatabaseHelper extends SQLiteOpenHelper{
     public void onCreate(SQLiteDatabase db) {
         db.execSQL(CREATE_BOOK);//调用exeSQL()方法去执行建表语句
         db.execSQL(CREATE_CATEGORY);
-        Toast.makeText(mContext, "Create succeeded", Toast.LENGTH_SHORT).show();
+        /*Toast.makeText(mContext, "Create succeeded", Toast.LENGTH_SHORT).show();*/
 
     }
 
